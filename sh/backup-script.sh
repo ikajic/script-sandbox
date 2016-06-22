de408cb639e931cedf153771c99f280d2b7fe496
Ivana Kajić <ivana.kajic@gmail.com>
2016-06-22 00:03:23 -0400

    Exclude multiple directories with rsync.

--- sh/backup-script.sh
+++ sh/backup-script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Basic script to rsync root and home
-
+# Basic script to rsync root and home, excluding media
+    
 echo 'Remounting backup as read-write'
 mount -o remount,rw /media/ivana/backup/
+
 echo 'Performing rsync backup of / and /home'
-rsync -avx --delete /       /media/ivana/backup/
-rsync -avx --delete /home/  /media/ivana/backup/home
+rsync -avx --delete --progress --stats  /       /media/ivana/backup/
+rsync -avx --delete --progress --stats --exclude-from='exclude.txt'    /home/    /media/ivana/backup/home
+
 echo 'Remounting BACKUP as read-only'
 mount -o remount,ro /media/ivana/backup
